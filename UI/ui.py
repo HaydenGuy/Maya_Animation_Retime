153ff43496f51d1e7c22c02418f9e0b1a3f00ca0
hayden <haydenguyn@gmail.com>
2025-01-19 12:16:06 -0800

    Added pushbuttons for increments

--- UI/ui.py
+++ UI/ui.py
@@ -1,4 +1,4 @@
-from PySide6.QtWidgets import QWidget, QVBoxLayout, QPushButton
+from PySide6.QtWidgets import QWidget, QVBoxLayout, QHBoxLayout, QPushButton, QSlider
 
 class Animation_Retime_UI(QWidget):
     def __init__(self):
@@ -8,6 +8,23 @@ class Animation_Retime_UI(QWidget):
 
     def setup_UI(self):
         main_layout = QVBoxLayout(self)
+        
+        button_layout = QHBoxLayout(self)
 
-        self.button = QPushButton("Button")
-        main_layout.addWidget(self.button)
+        self.neg_100_btn = QPushButton("-100")
+        self.neg_50_btn = QPushButton("-50")
+        self.neg_10_btn = QPushButton("-10")
+        self.neg_1_btn = QPushButton("-1")
+        self.zero_0_btn = QPushButton("0")
+        self.pos_1_btn = QPushButton("1")
+        self.pos_10_btn = QPushButton("10")
+        self.pos_50_btn = QPushButton("50")
+        self.pos_100_btn = QPushButton("100")
+        
+        buttons = [self.neg_100_btn, self.neg_50_btn, self.neg_10_btn, self.neg_1_btn,
+                   self.zero_0_btn, self.pos_1_btn, self.pos_10_btn, self.pos_50_btn, self.pos_100_btn]
+
+        for btn in buttons:
+            button_layout.addWidget(btn)
+
+        main_layout.addLayout(button_layout)
